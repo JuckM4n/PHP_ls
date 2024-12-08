f53e20ba8beb4b42fd997ef113d7dd726e4b8b5f
JuckM4n <nik_morozov0@mail.ru>
2024-12-08 22:10:53 +0300

    feat: third week's homework is done (without var_dump)

--- html/index.php
+++ html/index.php
@@ -2,9 +2,11 @@
 require '../vendor/autoload.php';
 require '../base/config.php';
 
+ini_set('display_errors', 'on');
+ini_set('error_reporting', E_ALL | E_NOTICE);
+
 $route = new \Base\Route();
 $route->add('/', \App\Controller\Login::class);
-$route->add('/register', \App\Controller\Login::class);
 
 $app = new \Base\Application($route);
 $app->run();
